6a3c3d90cf8f6de36187e638e5e65888eecf9d6a
debovema <mathieu.debove@fastconnect.fr>
2015-09-21 18:10:54 +0200

    Rafactoring: separation of maven-plugin from "core" JAR

--- fcunit-plugin/src/main/java/fr/fastconnect/factory/tibco/bw/fcunit/pages/UnitTestsIndexMojo.java
+++ fcunit-plugin/src/main/java/fr/fastconnect/factory/tibco/bw/fcunit/pages/UnitTestsIndexMojo.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package fr.fastconnect.tibco.businessworks.fcunit;
+package fr.fastconnect.factory.tibco.bw.fcunit.pages;
 
 import java.io.File;
 import java.io.IOException;
