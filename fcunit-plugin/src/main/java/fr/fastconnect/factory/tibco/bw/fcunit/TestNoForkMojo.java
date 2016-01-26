d1580e5f357a79f28e5e6789a53063ffcc377d7e
debovema <mathieu.debove@fastconnect.fr>
2016-01-26 11:56:43 +0100

    Avoid crash when no Designer5.prefs on the system

--- fcunit-plugin/src/main/java/fr/fastconnect/factory/tibco/bw/fcunit/TestNoForkMojo.java
+++ fcunit-plugin/src/main/java/fr/fastconnect/factory/tibco/bw/fcunit/TestNoForkMojo.java
@@ -135,6 +135,7 @@ public class TestNoForkMojo extends AbstractServiceEngineMojo {
 		// this try-block can be removed when BWMaven > 2.4.1 is released
 		try {
 			File systemDesigner5Prefs = new File(System.getProperty("user.home") + "/" + TIBCO_HOME_DIR + "/" + DESIGNER5_PREFS);
+			getLog().info(systemDesigner5Prefs.getAbsolutePath());
 			if (!systemDesigner5Prefs.exists()) {
 				systemDesigner5Prefs.createNewFile(); // touch the system file to avoid crash
 			}
